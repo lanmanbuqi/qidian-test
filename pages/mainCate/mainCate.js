8d6be7f734b3a72b23a6a3b6e156fe980d345966
YoCo <dengyongkang23@163.com>
2022-11-03 23:48:35 +0800

    第二次提交

--- pages/mainCate/mainCate.js
+++ pages/mainCate/mainCate.js
@@ -1,21 +1,36 @@
 $(function () {
 	var mySwiper = new Swiper('.swiper', {
 		loop: true, // 循环模式选项
+		autoplay: true,
 
 		// 如果需要分页器
 		pagination: {
 			el: '.swiper-pagination',
-		},
-
-		// 如果需要前进后退按钮
-		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
-		},
-
-		// 如果需要滚动条
-		scrollbar: {
-			el: '.swiper-scrollbar',
+			clickable: true,
+			paginationType: 'custom',
+			bulletClass: 'my-bullet', //需设置.my-bullet样式
+			bulletActiveClass: 'my-bullet-active',
+			bulletElement: 'div',
+			renderBullet: function (index, className) {
+				switch (index) {
+					case 0:
+						text = '光阴之外';
+						break;
+					case 1:
+						text = '最后的黑暗之王';
+						break;
+					case 2:
+						text = '我是导演，我不比';
+						break;
+					case 3:
+						text = '7号基地';
+						break;
+					case 4:
+						text = '起点百战沙尘';
+						break;
+				}
+				return '<div class="' + className + '">' + text + '</div>';
+			},
 		},
 	});
 });
